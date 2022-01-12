6b2da3cffab2e505ee1eff856042b6549522e935
Vitor Silva <vluis.silva1@gmail.com>
2022-01-12 20:38:52 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/services/ScoreService.java
@@ -0,0 +1,66 @@
+package com.devsuperior.dsmovie.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.Movie;
+import com.devsuperior.dsmovie.entities.Score;
+import com.devsuperior.dsmovie.entities.User;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+
+@Service
+public class ScoreService {
+
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ScoreRepository scoreRepository;
+	
+	@Transactional
+	public MovieDTO saveScore(ScoreDTO dto) {
+		
+		/** localiza o usuário por e-mail e valida se o mesmo já está cadastrado **/
+		User user = userRepository.findByEmail(dto.getEmail());
+		if (user == null) {
+			user = new User();
+			user.setEmail(dto.getEmail());
+			user = userRepository.saveAndFlush(user);
+		}
+		
+		/** seleciona o filme por id **/
+		Movie movie = movieRepository.findById(dto.getMovieId()).get();
+		
+		/** salva o score por usuário **/
+		Score score = new Score();
+		score.SetMovie(movie);
+		score.SetUser(user);
+		score.setValue(dto.getScore());
+		
+		score = scoreRepository.saveAndFlush(score);
+		
+		/** calculo da media de score do filme conforme as avaliações anteriores **/
+		double sum = 0.0;
+		for(Score s :movie.getScores()) {
+			sum = sum + s.getValue();
+		}
+		
+		double avg = sum / movie.getScores().size();
+		
+		movie.setScore(avg);
+		movie.setCount(movie.getScores().size());
+		
+		movie = movieRepository.save(movie);
+		
+		return new MovieDTO(movie);
+	}
+	
+}
